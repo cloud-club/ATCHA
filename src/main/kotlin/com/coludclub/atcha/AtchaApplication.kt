d51b4aa0013ae76c9fcd7ebc0cf73fe91489826e
ritty27 <jspark9812@gmail.com>
2023-06-25 17:19:04 +0900

    chore: server time zone setting

--- src/main/kotlin/com/coludclub/atcha/AtchaApplication.kt
+++ src/main/kotlin/com/coludclub/atcha/AtchaApplication.kt
@@ -2,10 +2,13 @@ package com.coludclub.atcha
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import java.util.TimeZone
 
 @SpringBootApplication
 class AtchaApplication
 
 fun main(args: Array<String>) {
+	TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"))
+
 	runApplication<AtchaApplication>(*args)
 }
